f575adbbdb50c3fec42a63142c29ecc6bd1647c0
AadilU <aadilu2002@gmail.com>
2023-04-30 01:36:55 -0700

    Added login page

--- Farm2ForkWebsite/src/components/pages/Login.tsx
+++ Farm2ForkWebsite/src/components/pages/Login.tsx
@@ -6,7 +6,33 @@ const Login = () => {
   return (
     <>
       <Navbar />
-      <h1>Login</h1>
+      <div className="container">
+        <form className="login-form">
+          <div className="mb-3">
+            <label className="form-label">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary" id="submit-button">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 };
